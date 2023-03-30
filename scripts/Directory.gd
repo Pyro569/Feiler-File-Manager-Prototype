4eec35a35f52776f25c22fc0f8b6b6790c935e36
Nathan M <poopybutface8@gmail.com>
2023-03-30 15:45:05 -0400

    Directory reading

---
+++ scripts/Directory.gd
@@ -0,0 +1,47 @@
+extends Node
+
+var directory = ""
+
+class File:
+	var name
+	
+	# 0 is unknown
+	# 1 is folder
+	# 2 is executable
+	var type
+	
+	# Size in MB
+	# TODO:
+	var size
+	
+	func _init(name, type):
+		self.name = name
+		self.type = type
+		self.size = 1
+	
+# includes folders too
+var filesInDirectory = []
+
+func update_dir_contents(path):
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				
+				filesInDirectory.push_back(File.new(file_name, 1))
+			else:
+				var file_type = (file_name.rsplit("."))
+				file_type = file_type[file_type.size() - 1]
+				match file_type:
+					"exe":
+						filesInDirectory.push_back(File.new(file_name, 2))
+					_:
+						filesInDirectory.push_back(File.new(file_name, 0))
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+
+func _ready():
+	update_dir_contents("C:/Users/nathan.mills/Documents/GitHub/Feiler-File-Manager/")
