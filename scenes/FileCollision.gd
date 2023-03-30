f6fede3a6c92030683935074770568b69e171133
Kobida <daniel.kobida@stu.campbell.kyschools.us>
2023-03-30 12:52:18 -0400

    funne powershell

--- scenes/FileCollision.gd
+++ scenes/FileCollision.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+var output = []
+
 func _on_mouse_entered():
 	print("mouse entered")
 	pass # Replace with function body.
@@ -10,4 +12,9 @@ func _on_mouse_exited():
 
 func _on_input_event(camera, event, position, normal, shape_idx):
 	print("input event", event)
+	#OS.execute("C:\\Windows\\System32\\WindowsPowerShell\\v1.0l\\powershell.exe", args, [], true)
+	OS.execute("CMD.exe", ["/C", "cd C:/ && START powershell.exe"], output, true, true)
+	print(output)
+
+
 
